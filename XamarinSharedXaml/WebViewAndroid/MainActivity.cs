35657f8824c8b1dcbf3e18e4487f85a7ee5d4ccf
Javier Bravo Conde <Javier Bravo Conde>
2017-01-29 23:41:54 -0600

    first commit

---
+++ XamarinSharedXaml/WebViewAndroid/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Android.OS;
+
+using WebViewAndroid.Models;
+
+namespace WebViewAndroid
+{
+    [Activity(Label = "WebViewAndroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            WebView.SetWebContentsDebuggingEnabled(true);
+
+            // Load the rendered HTML into the view with a base URL 
+            // that points to the root of the bundled Assets folder
+            webView.LoadUrl("file:///android_asset/index.html");
+
+        }
+    }
+}
+
---
+++ XamarinSharedXaml/WebViewAndroid/Models/Model1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebViewAndroid.Models
+{
+    public class Model1
+    {
+        public string Text { get; set; }
+    }
+}
